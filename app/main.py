f8bb7cbfc078805e28b34142a0309e21cba67616
Daniel Varoli <daniel.varoli@gmail.com>
2021-04-17 17:42:11 -0400

    use pydantic for data validation

--- app/main.py
+++ app/main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class CreateHaikuRequest(BaseModel):
+    starter_words: str
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/haiku/create")
-async def generate_haiku(starter_words: str):
-    w1, w2 = starter_words.split(",")
-
+@app.post("/haiku/create")
+async def generate_haiku(params: CreateHaikuRequest):
+    w1, w2 = params.starter_words.split(" ")
     response_body = {
         "starter_words": [w1, w2],
         "haiku": f"{w1.capitalize()} {w2} is all you need in life.",
