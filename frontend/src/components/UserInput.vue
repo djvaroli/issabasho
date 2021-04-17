344e5ec4c745acfbbe9c01853b6d4e1732ab9593
Daniel Varoli <daniel.varoli@gmail.com>
2021-04-17 17:41:31 -0400

    created Vue component for user input and added axios functionality

--- frontend/src/components/UserInput.vue
+++ frontend/src/components/UserInput.vue
@@ -1,15 +1,63 @@
 <template>
-  <div class="user-input">
-    <input type="text">
+  <div>
+    <div class="user-input-wrapper">
+      <input
+          type="text"
+          class="user-input"
+          placeholder="Blissful wind"
+          v-model="userInput"
+      >
+    </div>
+    <b-button
+        type="is-info"
+        @click="createNewHaiku"
+    >
+      Create Haiku
+    </b-button>
+    <div>
+      {{generatedHaiku}}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "UserInput.vue"
+  name: "UserInput.vue",
+  data() {
+    return {
+      "userInput": "",
+      "generatedHaiku": ""
+    }
+  },
+  methods: {
+      createNewHaiku() {
+        axios.post("http://localhost:8000/haiku/create", {
+          "starter_words": this.userInput
+        })
+        .then((response) => {
+          console.log(response);
+          this.generatedHaiku = response.data.haiku;
+        });
+      }
+    }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.user-input {
+  padding: 0.5rem;
+  margin: 1rem;
+  font-size: 3rem;
+  border: 0;
+  text-align: center;
+  border-bottom: 2px solid #333;
+  transition: 0.25s;
+  &:focus {
+      outline: none;
+      border-bottom: 2px solid red;
+      transition: 0.25s;
+    }
+}
 </style>
