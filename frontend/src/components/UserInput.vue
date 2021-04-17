62f59be595e585ea5ca4528ad7ecaa921b6bb549
Daniel Varoli <daniel.varoli@gmail.com>
2021-04-17 15:38:36 -0400

    added frontend repo

---
+++ frontend/src/components/UserInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user-input">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserInput.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
